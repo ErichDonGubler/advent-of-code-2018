74d4f8ce49d6edfc44545e7ea2af2be3bce4e047
Erich Gubler <erichdongubler@gmail.com>
2018-12-05 10:52:01 -0700

    Solve day 3

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,14 @@
 extern crate aoc_runner;
 extern crate aoc_runner_derive;
 extern crate arrayvec;
+extern crate re_parse;
+extern crate serde;
+extern crate serde_derive;
 
 use aoc_runner_derive::aoc_lib;
 
 pub mod day1;
 pub mod day2;
+pub mod day3;
 
 aoc_lib!{ year = 2018 }
